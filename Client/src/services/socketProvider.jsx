f5bdef9cf2443da690121a415669b428353655bf
Jephy Jayan Varghese <jephyjvarghese@gmail.com>
2025-02-03 12:59:32 +0530

    Update socketProvider.jsx

--- Client/src/services/socketProvider.jsx
+++ Client/src/services/socketProvider.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4848", {
+const socket = io("https://task-management-app-beryl-theta.vercel.app", {
   withCredentials: true,
 });
 
