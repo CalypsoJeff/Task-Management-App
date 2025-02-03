acf01e15255ac867114fa35ed87b3813435de46a
Jephy Jayan Varghese <jephyjvarghese@gmail.com>
2025-02-03 23:08:51 +0530

    Update otpModel.js

--- Server/models/otpModel.js
+++ Server/models/otpModel.js
@@ -1,24 +1,21 @@
-import mongoose, { Document, Schema } from "mongoose";
+const mongoose = require("mongoose");
 
-const otpSchema = new mongoose.Schema(
-  {
-    otp: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    generatedAt: {
-      type: Date,
-      default: Date.now,
-      expires: 300,
-    },
+const otpSchema = new mongoose.Schema({
+  otp: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+  },
+  generatedAt: {
+    type: Date,
+    default: Date.now,
+    expires: 300, // Expire after 5 minutes
+  },
+});
 
-module.exports = mongoose.model("OTP", otpSchema);
+const OTP = mongoose.model("OTP", otpSchema);
 
-
+module.exports = OTP;  // CommonJS export
