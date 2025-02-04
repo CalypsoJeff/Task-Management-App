17272740aeeb937b724b20259f4149f57ded3cc8
Jephy Jayan Varghese <jephyjvarghese@gmail.com>
2025-02-04 15:02:02 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const session = require("express-session");
 const dotenv = require("dotenv");
+const MongoStore = require('connect-mongo');
 const database = require("./config/DB");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
@@ -36,14 +37,25 @@ app.use((req, res, next) => {
 });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(
+//   session({
+//     secret: process.env.SECRET,
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 app.use(
   session({
     secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+      ttl: 3600, // 1 hour expiration time
+    }),
+    cookie: { secure: false, maxAge: 3600000 }, 
   })
 );
-
 app.options("*", cors());
 app.use((req, res, next) => {
   res.locals.user = req.session.user;
